651bc3a14eef90845950118a778d3c087695e3d9
Documenter.jl <documenter@juliadocs.github.io>
2025-02-25 14:11:05 +0000

    build based on a6ea4aa

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#MirrorVI.jl","page":"MirrorVI.jl","title":"MirrorVI.jl","text":"","category":"section"},{"location":"","page":"MirrorVI.jl","title":"MirrorVI.jl","text":"Documentation for MirrorVI.jl","category":"page"}]
+}

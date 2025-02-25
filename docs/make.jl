89265709f2ffb7330c6f88129bfc9b99ab861ef1
Marco Molinari <marco.molinari@medisin.uio.no>
2025-02-25 14:04:57 +0100

    editing docs generation

--- docs/make.jl
+++ docs/make.jl
@@ -4,12 +4,14 @@ using MirrorVI
 makedocs(
     sitename = "MirrorVI",
     format = Documenter.HTML(),
-    modules = [MirrorVI]
+    modules = [MirrorVI],
+    checkdocs = :exports
 )
 
 # Documenter can also automatically deploy documentation to gh-pages.
 # See "Hosting Documentation" and deploydocs() in the Documenter manual
 # for more information.
-#=deploydocs(
-    repo = "<repository url>"
-)=#
+deploydocs(
+    repo = "github.com/marcoelba/MirrorVI.jl",
+    devbranch = "main"
+)
